12f975355b223574ffde60d92d1a7907f6f82f06
RenatoHioji <okamoto.hioji@gmail.com>
2025-01-30 22:04:48 -0300

    first commit

---
+++ sunside/src/main/java/com/sunside_java/sunside/SunsideApplication.java
@@ -0,0 +1,13 @@
+package com.sunside_java.sunside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SunsideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SunsideApplication.class, args);
+	}
+
+}
---
+++ sunside/src/test/java/com/sunside_java/sunside/SunsideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunside_java.sunside;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunsideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
