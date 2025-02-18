5ae25bece350c11f85ead103d6759ac33ad22880
DSM5 <fatec-dsm5@academico.intra>
2025-02-18 19:49:57 -0300

    feat: removendo ''

--- src/main/java/com/sunside/SunsideApplication.java
+++ src/main/java/com/sunside/SunsideApplication.java
@@ -3,8 +3,7 @@ package com.sunside;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-''
-@SpringBootApplication
+g@SpringBootApplication
 public class SunsideApplication {
 
 	public static void main(String[] args) {
