0c10884d86496e0ddee102ab6faa545a88e3c264
DSM5 <fatec-dsm5@academico.intra>
2025-02-18 19:21:37 -0300

    feat: adicionando configuracao do swagger

--- src/main/java/com/sunside/controller/TestController.java
+++ src/main/java/com/sunside/controller/TestController.java
@@ -2,23 +2,17 @@ package com.sunside.controller;
 
 import com.sunside.utils.AWSUtils;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.util.UriBuilder;
+import org.springframework.web.bind.annotation.*;
 
-@Controller
+@RestController
 @RequestMapping("v1/test")
 @RequiredArgsConstructor
 public class TestController {
 
     private final AWSUtils awsUtils;
-    @PostMapping
+
+    @GetMapping
     ResponseEntity<String> saveFile(){
         // return ResponseEntity.status(HttpStatus.CREATED).body(awsUtils.saveFile(file.getName(), file));
         return ResponseEntity.ok().build();
