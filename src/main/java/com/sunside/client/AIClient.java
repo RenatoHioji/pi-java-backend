734541a5a8f12dba382d9f0f9da03fd3b889a2f7
DSM5 <fatec-dsm5@academico.intra>
2025-05-07 21:05:26 -0300

    fix: ajustando feign client

--- src/main/java/com/sunside/client/AIClient.java
+++ src/main/java/com/sunside/client/AIClient.java
@@ -1,13 +1,12 @@
 package com.sunside.client;
 
-import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(url="${aiclient.url")
+@FeignClient(name="AI-CLIENT", url="${aiclient.url")
 public interface AIClient {
 
     @PostMapping("classificacao")
-
     String searchCategory(MultipartFile image);
 }
