fdcee9e560a5000d331f1d45f7671775211afa42
RenatoHioji <okamoto.hioji@gmail.com>
2025-05-08 13:27:49 -0300

    fix: ajustando ai cleint

--- src/main/java/com/sunside/client/AIClient.java
+++ src/main/java/com/sunside/client/AIClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-@FeignClient(name="AI-CLIENT", url="${aiclient.url")
+@FeignClient(name="AI-CLIENT", url="${aiclient.url}")
 public interface AIClient {
 
     @PostMapping("classificacao")
