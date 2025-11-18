77aabdecb58254a02d5562112e58b0440364e0a8
RenatoHioji <okamoto.hioji@gmail.com>
2025-11-18 09:20:45 -0300

    fix: ajustando v2 inserts

--- src/main/resources/db/migration/V2___INSERT.sql
+++ src/main/resources/db/migration/V2___INSERT.sql
@@ -32,16 +32,16 @@ inserted_items AS (
         (gen_random_uuid(), 'Run', 'Run', 'correr.webp', 'correr.mp4', 'correr.mp3', 'action', 'verbs'),
         (gen_random_uuid(), 'Jump', 'Jump', 'pular.webp', 'pular.mp4', 'pular.mp3', 'action', 'verbs'),
         (gen_random_uuid(), 'Read', 'Read', 'ler.webp', 'ler.mp4', 'ler.mp3', 'action', 'verbs'),
-        (gen_random_uuid(), 'Feel', 'Feel', 'sinto.webp', 'sinto.mp4', 'sinto.mp3', 'action', 'verbs'),
+        (gen_random_uuid(), 'Feel', 'Feel', 'sinto.webp', 'sinto.mp4', 'feel.mp3', 'action', 'verbs'),
         (gen_random_uuid(), 'Draw', 'Draw', 'desenhar.webp', 'desenhar.mp4', 'desenhar.mp3', 'action', 'verbs'),
         (gen_random_uuid(), 'Write', 'Write', 'escrever.webp', 'escrever.mp4', 'escrever.mp3', 'action', 'verbs'),
         (gen_random_uuid(), 'Laugh', 'Laugh', 'rir.webp', 'rir.mp4', 'rir.mp3', 'action', 'verbs'),
         (gen_random_uuid(), 'Sing', 'Sing', 'cantar.webp', 'cantar.mp4', 'cantar.mp3', 'action', 'verbs'),
-        (gen_random_uuid(), 'Happy', 'Hap-py', 'feliz.webp', 'feliz.mp4', 'feliz.mp3', 'emotion', 'feelings'),
+        (gen_random_uuid(), 'Happy', 'Hap-py', 'feliz.webp', 'feliz.mp4', 'happy.mp3', 'emotion', 'feelings'),
         (gen_random_uuid(), 'Sad', 'Sad', 'triste.webp', 'triste.mp4', 'triste.mp3', 'emotion', 'feelings'),
         (gen_random_uuid(), 'Angry', 'An-gry', 'bravo.webp', 'bravo.mp4', 'bravo.mp3', 'emotion', 'feelings'),
         (gen_random_uuid(), 'Love', 'Love', 'amor.webp', 'amor.mp4', 'amor.mp3', 'emotion', 'feelings'),
-        (gen_random_uuid(), 'Hunger', 'Hun-ger', 'fome.webp', 'fome.mp4', 'fome.mp3', 'need', 'sensations'),
+        (gen_random_uuid(), 'Hunger', 'Hun-ger', 'fome.webp', 'fome.mp4', 'hungry.mp3', 'need', 'sensations'),
         (gen_random_uuid(), 'Want', 'Want', 'quero.webp', 'quero.mp4', 'quero.mp3', 'need', 'sensations'),
         (gen_random_uuid(), 'Dog', 'Dog', 'cachorro.webp', 'cachorro.mp4', 'cachorro.mp3', 'animal', 'mammals'),
         (gen_random_uuid(), 'Cat', 'Cat', 'gato.webp', 'gato.mp4', 'gato.mp3', 'animal', 'mammals'),
@@ -51,10 +51,10 @@ inserted_items AS (
         (gen_random_uuid(), 'Wood', 'Wood', 'madeira.webp', 'madeira.mp4', 'madeira.mp3', 'material', 'elements'),
         (gen_random_uuid(), 'Duck', 'Duck', 'pato.webp', 'pato.mp4', 'pato.mp3', 'animal', 'birds'),
         (gen_random_uuid(), 'Yes', 'Yes', 'sim.webp', 'sim.mp4', 'sim.mp3', 'response', 'affirmative'),
-        ('2e6cc0da-8c57-4e19-ae02-9228ce74bdd2', 'Grape', 'Grape', 'uva.webp', 'uva.mp4', 'uva.mp3', 'food', 'fruits'),
-        ('5bb4d924-fdfc-4de4-9af2-efb69f102d7f', 'Apple', 'Ap-ple', 'maca.webp', 'maca.mp4', 'maca.mp3', 'food', 'fruits'),
-        ('7f8e3e4d-0f71-4c9d-9bd3-6c42eb5d6af0', 'Lemon', 'Le-mon', 'limao.webp', 'limao.mp4', 'limao.mp3', 'food', 'fruits'),
-        ('c1a34f5b-91d8-4e34-bc32-0ce0bf912b94', 'Pineapple', 'Pine-ap-ple', 'abacaxi.webp', 'abacaxi.mp4', 'abacaxi.mp3', 'food', 'fruits')
+        ('2e6cc0da-8c57-4e19-ae02-9228ce74bdd2', 'Grape', 'Grape', 'uva.webp', 'grape.mp4', 'grape.mp3', 'food', 'fruits'),
+        ('5bb4d924-fdfc-4de4-9af2-efb69f102d7f', 'Apple', 'Ap-ple', 'maca.webp', 'apple.mp4', 'apple.mp3', 'food', 'fruits'),
+        ('7f8e3e4d-0f71-4c9d-9bd3-6c42eb5d6af0', 'Lemon', 'Le-mon', 'limao.webp', 'lemon.mp4', 'lemon.mp3', 'food', 'fruits'),
+        ('c1a34f5b-91d8-4e34-bc32-0ce0bf912b94', 'Pineapple', 'Pine-ap-ple', 'abacaxi.webp', 'pineapple.mp4', 'pineapple.mp3', 'food', 'fruits')
     RETURNING id
 )
 , numbered_items AS (
